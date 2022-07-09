457e5d8ca721627f6719c7141153f5044464535d
RAM <srpatil9944@gmail.com>
2022-07-10 00:01:45 +0530

    first repos

---
+++ src/main/java/git_gith/github1.java
@@ -0,0 +1,12 @@
+package git_gith;
+
+public class github1 {
+public static void main(String[] args) {
+	String s="RAM";
+	if (s=="RAM")
+	System.out.println("YES RAM");
+	else {
+		System.out.println("MARSHIVANE");
+	}
+}
+}
