41f600ef1171ba9407f8078ebb8908f2315723a8
RAM <srpatil9944@gmail.com>
2022-07-10 02:02:15 +0530

    added calender code

---
+++ src/main/java/git_gith/CalenderHandling.java
@@ -0,0 +1,25 @@
+package git_gith;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CalenderHandling {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("Webdriver.chromedriver", "C:\\Users\\RAM V. MARSHIVANE\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();	
+		String url="https://www.redbus.in/";
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(2));
+		driver.findElement(By.id("src")).sendKeys("ramm");
+		driver.findElement(By.xpath("//div[@class='fl search-box date-box gtm-onwardCalendar']")).click(); 
+		driver.findElement(By.xpath("//table/tbody/tr[5]/td[5]")).click();
+	}
+
+}
